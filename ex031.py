dc143d348683bea978c5589329f3677ee8def75d
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-29 14:40:40 -0300

    more

--- ex031.py
+++ ex031.py
@@ -1,3 +1,10 @@
 v = int(input("Qual a distãncia de uma viagem? "))
 
+
 if v <= 200:
+     m = (v * 0.50)
+     print("O preço da passagem é R$ {:.2f}".format(m))
+
+else :
+    m = (v * 0.45)
+    print("O preço da passagem foi R$ {:.2f}".format(m))
---
+++ ex033.py
@@ -0,0 +1,7 @@
+n1 = int(input("digite o primeiro número: "))
+n2 = int(input("Digite o segundo: "))
+n3 = int(input("Digite o terceiro: "))
+
+if n1 > n2 and n1 > n3:
+    print("{} é maior que {} e {}".format(n1, n2, n3))
+
