53d4036f9376fad7eba7519041bc399d46016472
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-04-28 16:04:56 -0300

    more

--- ex045.py
+++ ex045.py
@@ -7,3 +7,15 @@ lista = ["pedra", "papel", "tesoura"]
 sorteando = choice(lista)
 print("Sua escolha {} ".format(j))
 print("Computador jogou {}".format(sorteando))
+
+if j == sorteando:
+    print("EMPATE")
+elif j == "pedra"  and sorteando =="tesoura":
+    print("JOGADOR VENCE")
+elif j == "papel" and sorteando == "pedra":
+    print("JOGADOR VENCE")
+elif j == "tesoura" and sorteando == "papel":
+    print("JOGADOR VENCE")
+else:
+    print("O computador venceu")
+
