5fb5b03306c235da7807456192eb48889a47584b
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-05-10 11:18:54 -0300

    version of code

--- ex050.py
+++ ex050.py
@@ -1,10 +1,22 @@
-n1 = int(input("Digite o primeiro numero: "))
-n2 = int(input("Digite o segundo numero: "))
-n3 = int(input("Digite o terceiro numero: "))
-n4 = int(input("Digite o quarto numero: "))
-n5 = int(input("Digite o ultimo numero: "))
-n6 = int(input("Digite o ultimo numero: "))
+n1 = int(input("Qual o primeiro valor: "))
+n2 = int(input("Qual o segundo valor: "))
+n3 = int(input("Qual o terceiro valor: "))
+n4 = int(input("Qual o quarto valor: "))
+n5 = int(input("Qual o quinto valor: "))
+n6 = int(input("Qual o sexto valor: "))
+for c in range(0,7):
+    if n1 % 2 == 0:
+        print("{} é par".format(n1))
+        if n2 % 2 == 0:
+          print("{} é par".format(n2))
+          if n3 % 2 == 0:
+              print("{} é par".format(n3))
+              if n4 % 2 == 0:
+                  print("{} é par".format(n4))
+                  if n5 % 2 == 0:
+                      print("{} é par".format(n5))
+                      if n6 % 2 == 0:
+                          print("{} é par".format(n6))
 
-for c in range(1,6):
-    if c % 2 == 0:
-        print()
+print("Fim")
+
