64c70d6b1cd2dbeccfd20db7603a69bf2c60e819
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-05-13 15:55:22 -0300

    correct of exercises

--- ex052.py
+++ ex052.py
@@ -1,1 +1,15 @@
 a1 = int(input("Digite um número inteiro: "))
+tot = 0
+
+for c in range(1, a1 + 1):
+    if a1 % c == 0:
+        print("\033[33m", end=" ")
+        tot += 1
+    else :
+        print("\033[31m", end=" ")
+    print("{} ".format(c), end=" ")
+print("\n\033[mO número {} foi dívisivel por {} vezes".format(a1,tot))
+if tot == 2:
+    print("Por isso ele é PRIMO!")
+else:
+    print("Por isso ele não é PRIMO!")
--- ex053.py
+++ ex053.py
@@ -1,3 +1,12 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
-
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+'''for letra in range(len(junto)-1,-1,-1):
+   inverso += junto[letra]'''
+print("O inverso de {} é {}".format(junto,inverso))
+if inverso == junto:
+    print('Temos um palindromo')
+else:
+    print("Não temos um palindromo")
 
