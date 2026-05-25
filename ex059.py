d5b37c21bc20e132332a621ef86f96239111fdc3
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-05-25 14:19:18 -0300

    version

--- ex059.py
+++ ex059.py
@@ -1,20 +1,33 @@
 opcao = 0
+print(''''Você quer?
+             [1] somar
+             [2] multiplicar
+             [3] maior
+             [4] novos numeros
+             [5] sair do programa''')
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+opcao = str(input("Qual a sua opção?"))
 while opcao != 5:
-     valor1 = int(input("Digite o primeiro valor: "))
-     valor2 = int(input("Digite o segundo valor: "))
-     opcao = str(input("Qual a sua opção?"))
-     print(''''Você quer?
-              [1] somar
-              [2] multiplicar
-              [3] maior
-              [4] novos numeros
-              [5] sair do programa''')
-     if opcao == 1:
+
+    if opcao == 1:
          print("{} + {} = {}".format(valor1, valor2, valor1 + valor2))
      if opcao == 2:
          print("{} * {} = {}".format(valor1, valor2, valor1 * valor2))
      if opcao == 3:
-     
+         print("{} / {} = {}".format(valor1, valor2, valor1 / valor2))
+     if opcao == 4:
+         print("Você quer entrar com novos numeros?")
+         r = str(input("Quer continuar?"))
+         if r == "Ss":
+             valor1 = int(input("Digite o primeiro valor: "))
+             valor2 = int(input("Digite o segundo valor: "))
+         elif r == "Nn":
+             print("OK")
+             break
+     if opcao == 5:
+         print("Saindo do programa")
+         break
 
 
 
