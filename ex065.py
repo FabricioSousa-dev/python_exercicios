41b0c2f7bbeb9299d9bca6111ea698921806d526
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-01 15:19:17 -0300

    version of code

--- ex065.py
+++ ex065.py
@@ -1,3 +1,26 @@
+n = " "
+cont = soma = 0
+media = 0
+maior = menor = 0
+
+while True:
+    cont += 1
+    n =int(input("Digite um valor: "))
+    soma += n
+
+    opcao = str(input("Quer continuar? [S/N] ")).strip().upper()
+
+
+    if opcao == "N":
+        break
+if cont > 0:
+    media = soma / cont
+else:
+    media = 0
+
+print("Você digitou {} números,a média entre eles foi {:.2f} ".format(cont,media))
+
+
 
 
 
