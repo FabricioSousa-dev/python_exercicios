5aab9545bd69aaaccc1728e1a8b996374f846ab6
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-01 16:02:35 -0300

    altenative resolution

--- ex065.py
+++ ex065.py
@@ -1,7 +1,7 @@
-n = " "
-cont = soma = 0
+'''n = " "
+cont = soma = maior = menor = 0
 media = 0
-maior = menor = 0
+
 
 while True:
     cont += 1
@@ -9,6 +9,14 @@ while True:
     soma += n
 
     opcao = str(input("Quer continuar? [S/N] ")).strip().upper()
+    if cont == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+
 
 
     if opcao == "N":
@@ -17,8 +25,29 @@ if cont > 0:
     media = soma / cont
 else:
     media = 0
+print("Tem {} valores digitados, a média foi {}".format(cont,media))
+print("O maior número digitado foi {} é o menor foi {}".format(maior,menor))
+'''
+#2 solução:
 
-print("Você digitou {} números,a média entre eles foi {:.2f} ".format(cont,media))
+resp = "s"
+soma = quant = maior = menor = 0
+while resp in "Ss":
+    num = int(input("Digite um número: "))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input("Quer continuar? [S/N] ")).strip().upper()[0]
+
+média = soma / quant
+print("Tem {} valores digitados, a média foi {}".format(quant,média))
+print("O maior número digitado foi {} é o menor foi {}".format(maior,menor))
 
 
 
