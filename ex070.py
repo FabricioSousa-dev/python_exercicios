7a25c52bbdef78a1f76f2b151f4102c1184bff82
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-06 17:08:26 -0300

    version

--- ex070.py
+++ ex070.py
@@ -1,35 +1,24 @@
-totpreço = maisprodutos =  0
-menorPreço = float("inf")
-nomeMaisbarato = ' '
+total = mais = menor = cont =  0
+barato = ' '
 while True:
-    print('-'*30)
-    print("LOJA SUPER BARATÃO")
-    print('-'*30)
+    produto = str(input('Digite o nome do produto: '))
+    preco = float(input('Digite o valor do produto: '))
+    cont += 1
 
+    total += preco
 
-
-
-    nome = str(input('Qual o nome do produto? '))
-    preço = float(input("Qual o preço do produto? R$  "))
-
-
-
-    totpreço += preço
-
-    if preço >= 1000:
-        maisprodutos += 1
-    if preço < menorPreço:
-        menorPreço = preço
-        nomeMaisbarato = nome
-
-
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    if preco > 1000:
+        mais += 1
 
     resp = ' '
-    while resp not in "SN":
+    while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
+print(f'O total da compra foi R${total}')
+print(f"{mais} produto(s) tem um preço mais que 1000")
+print(f"O produto mais barato foi {barato} custa R${menor:.2f}")
 
-print(f"O total gasto na compra é {totpreço} ")
-print(f"Tem {maisprodutos} produtos que custam mais de R$1000 ")
-print(f"O produto mais barato é {nomeMaisbarato} com o menor preço de {menorPreço} R$")
