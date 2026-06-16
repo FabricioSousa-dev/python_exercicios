4548559b088a945fcf9a94ce68fbebcc492372ab
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-15 22:16:01 -0300

    more

---
+++ ex072.py
@@ -0,0 +1,11 @@
+contagem = ("zero","um","dois","três","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove","vinte")
+
+p = int(input("Digite um numero entre 0 e 20: "))
+
+print(f"O número digitado foi o {contagem[p]} ")
+
+
+
+
+
+
