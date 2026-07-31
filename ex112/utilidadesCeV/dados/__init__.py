00d4fa5fb3ff7f0c26d97bc1ed90964e324f54c6
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-07-31 16:38:40 -0300

    corect logic

--- ex112/utilidadesCeV/dados/__init__.py
+++ ex112/utilidadesCeV/dados/__init__.py
@@ -11,15 +11,14 @@ def leiaDinheiro(mgs):
                 print("Erro, digite apenas um número")
             if p.strip() == '':
                 print("Erro! valor invalido!")
-            if '.' in p:
-                p = p.replace('.', '.')
+            if ',' in p:
+                p = p.replace(',', '.')
                 valor = float(p)
                 ok = True
             if ',' in p:
-                p = p.replace(',',',')
+                p = p.replace(',','.')
                 valor = float(p)
                 ok = True
-
         if ok:
             break
     return valor
